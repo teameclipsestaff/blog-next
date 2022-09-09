73009cfb77b015836cad319e8a3417afa0857de5
teameclipsestaff <85002438+teameclipsestaff@users.noreply.github.com>
2022-09-09 19:04:29 -0400

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -13,7 +13,7 @@ export default function Post({ post }) {
 
 // tell next.js how many pages there are
 export async function getStaticPaths() {
-  const res = await fetch("http://localhost:1337/posts");
+  const res = await fetch("https://strapiadminayden.herokuapp.com/posts");
   const posts = await res.json();
 
   const paths = posts.map((post) => ({
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const { slug } = params;
 
-  const res = await fetch(`http://localhost:1337/posts?Slug=${slug}`);
+  const res = await fetch(`https://strapiadminayden.herokuapp.com/posts?Slug=${slug}`);
   const data = await res.json();
   const post = data[0];
 
